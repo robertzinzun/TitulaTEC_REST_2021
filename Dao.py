23c0becaaf7b85c1f06d2088e825d1910d33fc06
robertzinzun <robertzinzun@gmail.com>
2021-10-30 10:52:31 -0500

    Optimización de la generación de json
    Modificación de los métodos encargados de generar el JSON

--- Dao.py
+++ Dao.py
@@ -13,41 +13,42 @@ class Opcion(db.Model):
     def consultaGeneral(self):
         dict_json = {"estatus": "", "mensaje": "", "opciones": []}
         try:
-
-            dict_json=self.to_json_list(self.query.all())
-
+            dict_json['estatus'] = "Ok"
+            lista = self.query.all()
+            if len(lista)>0:
+                dict_json['mensaje'] = "Listado de opciones"
+                dict_json['opciones'] = self.to_json_list(lista)
+            else:
+                dict_json['mensaje'] = "No hay opciones registradas"
         except:
-            dict_json['estatus']="Error"
-            dict_json['mensaje']="Error al ejecutar la consulta de opciones"
+            dict_json['estatus'] = "Error"
+            dict_json['mensaje'] = "Error al ejecutar la consulta de opciones"
 
         return json.dumps(dict_json)
 
-    def to_json_list(self,lista):
-        dict_json = {"estatus": "ok", "mensaje": "Listado de Opciones", "opciones": []}
-        lista_opciones=[]
+    def to_json_list(self, lista):
+        lista_opciones = []
         for o in lista:
-            dict_opcion={"idOpcion":o.idOpcion,"nombre":o.nombre,"descripcion":o.descripcion}
-            lista_opciones.append(dict_opcion)
-        dict_json['opciones']=lista_opciones
-        if len(lista_opciones)==0:
-            dict_json['mensaje']='No hay opciones registradas'
-        return dict_json
+            lista_opciones.append(self.to_json(o))
+
+        return lista_opciones
 
     def consultaIndividual(self,id):
         dict_json = {"estatus": "", "mensaje": "", "opcion":{}}
         try:
-            dict_json = self.to_json(self.query.get(id))
+            dict_json['estatus'] = "Ok"
+            dict_json['mensaje'] = "Listado de la opcion"
+            dict_json['opcion'] = self.query.get(id).to_json()
         except:
             dict_json['estatus'] = "Error"
             dict_json['mensaje'] = "Error al ejecutar la consulta de la opcion"
 
         return json.dumps(dict_json)
 
-    def to_json(self,o):
-        dict_json = {"estatus": "ok", "mensaje": "Listado de la opcion", "opcion": {}}
-        opcion = {"idOpcion": o.idOpcion, "nombre": o.nombre, "descripcion": o.descripcion}
-        dict_json['opcion']=opcion
-        return dict_json
+    def to_json(self):
+        opcion = {"idOpcion": self.idOpcion, "nombre": self.nombre, "descripcion": self.descripcion}
+        return opcion
+
     def insertar(self,json):
         self.from_json(json)
         db.session.add(self)
